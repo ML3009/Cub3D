009ea2be4fce3135e2cf77d992fd31ea5b27fc59
Marie-Laure Vautrot <vautrot.marielaure@gmail.com>
2023-09-07 11:36:28 +0200

    start

--- include/struct.h
+++ include/struct.h
@@ -1,18 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   struct.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mvautrot <mvautrot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/06 15:20:14 by mvautrot          #+#    #+#             */
-/*   Updated: 2023/09/07 11:28:50 by mvautrot         ###   ########.fr       */
+/*   Created: 2023/09/07 11:29:06 by mvautrot          #+#    #+#             */
+/*   Updated: 2023/09/07 11:36:02 by mvautrot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main(int ac, char **av)
-{
-	
+#include "../library/libft/include/libft.h"
 
-	return (0);
-}
+
+typedef struct s_vector
+{
+	int	x;
+	int	y;
+}	t_vector;
+
+
+typedef struct s_player
+{
+	t_vector	pos;
+}	t_player;
+
+
+
+typedef struct s_map
+{
+	char	**map;
+	t_player	player;
+
+}	t_map;
+
+
+
