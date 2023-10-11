b1c6aec189d25e5f5372ce830758a8f2ca08c1d3
Marie-Laure Vautrot <vautrot.marielaure@gmail.com>
2023-10-11 12:42:35 +0200

    mouse_close

--- src/game/mouse.c
+++ src/game/mouse.c
@@ -6,7 +6,7 @@
 /*   By: mvautrot <mvautrot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 17:22:03 by mvautrot          #+#    #+#             */
-/*   Updated: 2023/10/11 12:05:33 by mvautrot         ###   ########.fr       */
+/*   Updated: 2023/10/11 12:41:51 by mvautrot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,9 @@ void mouse_pov(t_data *data)
 	int	pos_y;
 
 	mlx_mouse_get_pos(data->mlx.mlx_id, data->mlx.mlx_window, &pos_x, &pos_y);
-	if (pos_x <= ((int)data->mlx.size.x / 3))
+	if (pos_x <= ((int)data->mlx.size.x / 3) && (pos_y >= data->mlx.size.y / 10))
 		data->key.vleft = 1;
-	else if (pos_x >= (((int)data->mlx.size.x / 3) * 2))
+	else if (pos_x >= (((int)data->mlx.size.x / 3) * 2) && (pos_y >= data->mlx.size.y / 10))
 		data->key.vright = 1;
 	else
 	{
@@ -29,3 +29,5 @@ void mouse_pov(t_data *data)
 	}
 	return ;
 }
+
+// creer une limite avec y pour fermeture
